520e72675ddb7e2de0211f89baedae18f70e11ce
anitasnow9 <42756969+anitasnow9@users.noreply.github.com>
2019-11-20 02:28:41 -0500

    Update PythonCode.py

--- PythonCode.py
+++ PythonCode.py
@@ -1,69 +1,47 @@
-Main.java
-import java.sql.*;
-import java.util.ArrayList;
+import MySQLdb
 
-public class Main
+class DBConnection:
+    def __init__(self, DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME):
+        self.host = DB_HOST
+        self.port = DB_PORT
+        self.user = DB_USER
+        self.password = DB_PASSWORD
+        self.name = DB_NAME
+        self.conn = None
+        
+    def get_conn(self):
+        self.conn = MySQLdb.connect(host = self.host,
+                                    port = self.port,
+                                    db = self.name,
+                                    user = self.user,
+                                    passwd = self.password)
+    
+    mydbconnobj = DBConnection('localhost',3306,'admin','adm123','sakila')
+    mydbconn = mydbconnobj.get_conn()
+ 
 
-    public static void main(String[] args)
-        Person person = new Person(Barrack,Obama,      12, 5136112,12345);
-        Person person1 = new Person(Name1, LastName6, 13, 5136113,12346);
-        Person person2 = new Person(Name2, LastName7, 14, 5136114,12347);
-        Person person3 = new Person(Name3, LastName8, 15, 5136115,12348);
-        Person person4 = new Person(Name4, LastName9, 16, 5136116,12349);
-        Person person5 = new Person(Name5, LastName10, 17, 5136117,12350);
+class Actor
+    def __init__(self, actor_id, first_name, last_name, last_update):
+        self.actor_id = actor_id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.last_update = last_update
 
+act_1 = Actor(NULL,'Ning','Wang','2019-11-19')
+act_2 = Actor(NULL,'User','Test','2019-11-19')
+    
+add_actor = ("INSERT INTO actor "
+             "(actor_id, first_name, last_name, last_update) "
+             "VALUES (%s, %s, %s, %s)")
+    
+cursor.execute(add_actor, act_1)
+cursor.execute(add_actor, act_2)
+act_no = cursor.lastrowid
 
-        createTable();
-        insertPerson(person);
-        insertPerson(person1);
-        insertPerson(person2);
-        insertPerson(person3);
-        insertPerson(person4);
-        insertPerson(person5);
+connection.commit()
+connection.close()
 
-        System.out.println(selectPerson(4));
-        for(Person p: findAllPeople())
-            System.out.println(p);
-       
-        deletePerson(4);
-
-        for(Person p: findAllPeople())
-            System.out.println(p);
-       
-
-   
-
-    public static Connection deletePerson(int id)
-        Connection connection = null;
-        Statement stmt = null;
-
-        try
-            connection = getConnection();
-            connection.setAutoCommit(false);
-
-            stmt = connection.createStatement();
-            String sql = DELETE from MARK where ID= + id + ;;
-            stmt.executeUpdate(sql);
-            connection.commit();
-            connection.close();
-
-            System.out.println((Deleted Person + id + ) done successfully);
-        catch ( Exception e )
-            System.out.println(e);
-            connection = null;
-       
-        return connection;
+sql_command = """SELECT * FROM actor WHERE actor_id = act_no"""
+cursor.execute(sql_command)
 
 
-   
-
-    public static ArrayList findAllPeople()
-        Connection connection = null;
-        Statement statement = null;
-        ArrayList person = new ArrayList<>();
-
-        try
-            connection = getConnection();
-            connection.setAutoCommit(false);
-
-            statement = connection.createStatement();
